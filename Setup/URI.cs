66b4f3c2fdcd7fb813b17da0a5e0096caf8a7c6b
Bluscream <admin@timo.de.vc>
2019-05-26 09:15:22 +0200

    uwu

---
+++ Setup/URI.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using Microsoft.Win32;
+using ETS2Launcher.Utils;
+
+namespace VRChatLauncher.Setup
+{
+    class URI
+    {
+        public const string key = @"HKEY_CLASSES_ROOT\ets2\shell\open\command";
+        public static URIResponse CheckURIRegistryKey()
+        {
+            var ret = new URIResponse(URIResponse.URIEnum.UNKNOWN, "", "");
+            ret.key = (string)Registry.GetValue(key, null, null);
+            if (ret.key == null) return ret;
+            // Logger.Trace("key=", ret.key);
+            var steamFile =Path.Combine(Utils.getOwnPath().DirectoryName, "launch.bat");
+            var expected_steam = $@"""{steamFile}"" ""%1"""; // ""{ownPath}"" 
+            // Logger.Trace("expected_steam=", expected_steam);
+            if (expected_steam == ret.key) { ret.match = URIResponse.URIEnum.DEFAULT; return ret; }
+            ret.expected = $@"""{Utils.getOwnPath().FullName}"" ""%1"""; // ""{ownPath}"" 
+            // Logger.Trace("expected=", ret.expected);
+            if (ret.key != ret.expected) ret.match = URIResponse.URIEnum.WRONG;
+            else ret.match = URIResponse.URIEnum.INSTALLED;
+            Logger.Debug(ret.ToJson());
+            // "G:\Steam\steamapps\common\VRChat\launch.bat" "G:\Steam\steamapps\common\VRChat" "%1"
+            return ret;
+        }
+        public static bool InstallURI(string val = null)
+        {
+            if (val is null)
+            {
+                var ownPath = Path.GetDirectoryName(Application.ExecutablePath);
+                var ownFile = Path.GetFullPath(Application.ExecutablePath);
+                val = $@"""{ownFile}"" ""{ownPath}"" ""%1""";
+            }
+            Logger.Debug(val);
+            try { Registry.SetValue(key, null, val);
+            } catch (Exception) { return false; }
+            return true;
+        }
+    }
+    public class URIResponse {
+        public URIResponse(URIEnum item1, string item2, string item3) {
+            match = item1; expected = item2; key = item3;
+        }
+        public URIEnum match { get; set; }
+        public string expected { get; set; }
+        public string key { get; set; }
+        public enum URIEnum {
+            INSTALLED, DEFAULT, WRONG, UNKNOWN
+        }
+    }
+}
